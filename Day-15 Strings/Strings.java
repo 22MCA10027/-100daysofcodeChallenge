d7f867cf897142fadbfe12f59788163af3124b1b
Singanamala HariKrishna <114754868+22MCA10027@users.noreply.github.com>
2022-10-23 21:53:19 +0530

    Create Strings.java

---
+++ Day-15 Strings/Strings.java
@@ -0,0 +1,15 @@
+class Strings
+{
+   public static void main(String[] args)
+    {
+        //Creating Strings
+        String firstname = "Hari";
+        String lastname = "Krishna";
+        String surename = "Singanamala";
+    
+       //Printing Strings
+        System.out.println(firstname);
+        System.out.println(lastname);
+        System.out.println(surename);
+    }
+}
