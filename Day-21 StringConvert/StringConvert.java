78ae5521293be93c9ed90df3a52178f3581535aa
Singanamala HariKrishna <114754868+22MCA10027@users.noreply.github.com>
2022-10-29 23:54:03 +0530

    Create StringConvert.java

---
+++ Day-21 StringConvert/StringConvert.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class StringConvert 
+{
+    public static void main(String[] args)
+    {
+        String input1 = "INDIA IS MY COUMTRY";
+        String input2 = "i love my country";
+        System.out.println(input1);
+        System.out.println(input1.toLowerCase());
+        System.out.println(input2);
+        System.out.println(input2.toUpperCase());
+
+    }
+}
