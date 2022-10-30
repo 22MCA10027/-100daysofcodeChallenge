454dbcf61b241dceadf1839e5c6c0894cbbb57b8
Singanamala HariKrishna <114754868+22MCA10027@users.noreply.github.com>
2022-10-30 23:12:40 +0530

    Create StringReplace.java

---
+++ Day-22  StringReplace/StringReplace.java
@@ -0,0 +1,15 @@
+ class StringReplace
+
+ {
+
+    public static void main(String[] args)
+
+    {
+        String str = "Java Programming";
+
+        String newString = str.replace('a', 'A');
+
+        System.out.println(newString); 
+    }
+    
+ }
