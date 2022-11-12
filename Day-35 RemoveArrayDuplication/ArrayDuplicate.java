efde5d9f0b3b4829ff6410e6223e71bae505f78c
Singanamala HariKrishna <114754868+22MCA10027@users.noreply.github.com>
2022-11-12 19:41:42 +0530

    Create ArrayDuplicate.java

---
+++ Day-35 RemoveArrayDuplication/ArrayDuplicate.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class ArrayDuplicate
+{
+   public static void main(String[] args) 
+   {
+      int arr[] = {10, 10, 20, 20, 30, 30, 40, 40, 50, 50, 60, 60, 60};
+      
+      int newArr[] = removeDuplicates(arr);
+      
+      System.out.println("Original array: "+ Arrays.toString(arr));
+      System.out.println("After removing duplicates: " + Arrays.toString(newArr));
+        }
+   public static int[] removeDuplicates(int[] arr)
+   {
+        int j = 0; // index without duplicates
+      int lastIndex = arr.length-1; 
+         for (int i = 0; i < lastIndex; i++)
+         {
+         if(arr[i] != arr[i+1]) 
+         {
+            arr[j++] = arr[i];
+         }
+      }
+      if(arr[j] != arr[lastIndex])
+         arr[j++] = arr[lastIndex];
+      return Arrays.copyOf(arr, j);
+   }
+}
