f4ebb7295d0148549a80d8d0f91df56e1ee7df66
Singanamala HariKrishna <114754868+22MCA10027@users.noreply.github.com>
2022-11-25 18:07:09 +0530

    Create SumofSeries.java

---
+++ Day-48 SumofSeries/SumofSeries.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SumofSeries
+ {
+  public static void main(String[] args)
+   {
+
+    Scanner sc = new Scanner(System.in);
+
+    int n;
+    int sum = 0;
+
+    System.out.print("Enter N value: ");
+    n = sc.nextInt();
+
+    for(int i=1; i<=n; i++) 
+    {
+      sum = sum + i;
+    }
+
+    System.out.println("Sum of Series is = " + sum);
+  }
+}
