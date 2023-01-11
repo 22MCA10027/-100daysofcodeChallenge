d686f5eaa1ef6217bbab62a011b5b392868ab17a
Singanamala HariKrishna <114754868+22MCA10027@users.noreply.github.com>
2023-01-11 11:52:48 +0530

    Create FirstSwingExample.java

---
+++ Day-84 Swings/FirstSwingExample.java
@@ -0,0 +1,20 @@
+import javax.swing.*;  
+
+public class FirstSwingExample
+ {  
+public static void main(String[] args) 
+{  
+
+JFrame f=new JFrame(); 
+          
+JButton b=new JButton("click");
+b.setBounds(130,100,100, 40); 
+          
+f.add(b); 
+          
+f.setSize(400,500);
+f.setLayout(null);
+f.setVisible(true);  
+
+}  
+}  
