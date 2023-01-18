b344081bd368af05e1f3d370ea2c9be47965a68b
Singanamala HariKrishna <114754868+22MCA10027@users.noreply.github.com>
2023-01-18 09:29:04 +0530

    Create PriorityQueueCollection.java

---
+++ Day-91 Collection/PriorityQueueCollection.java
@@ -0,0 +1,28 @@
+import java.util.*;  
+public class PriorityQueueCollection
+{  
+public static void main(String args[])
+{  
+PriorityQueue<String> queue=new PriorityQueue<String>();  
+queue.add("Aadarsh");  
+queue.add("Vignesh");  
+queue.add("Kishore");  
+queue.add("Pawan");  
+System.out.println("head:"+queue.element());  
+System.out.println("head:"+queue.peek());  
+System.out.println("iterating the queue elements:");  
+Iterator itr=queue.iterator();  
+while(itr.hasNext())
+{  
+System.out.println(itr.next());  
+}  
+queue.remove();  
+queue.poll();  
+System.out.println("after removing two elements:");  
+Iterator<String> itr2=queue.iterator();  
+while(itr2.hasNext())
+{  
+System.out.println(itr2.next());  
+}  
+}  
+}  
