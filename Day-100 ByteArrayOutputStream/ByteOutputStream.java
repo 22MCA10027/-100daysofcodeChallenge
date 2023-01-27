bd9bd5a1089c0a2f30e8d177448b41f552ddfefa
Singanamala HariKrishna <114754868+22MCA10027@users.noreply.github.com>
2023-01-27 16:57:19 +0530

    Create ByteOutputStream.java

---
+++ Day-100 ByteArrayOutputStream/ByteOutputStream.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+class ByteOutputStream 
+{
+
+public static void main(String args[]) throws IOException
+{
+
+ByteArrayOutputStream f = new ByteArrayOutputStream();
+String s = "This should end up in the array";
+byte buf[] = s.getBytes();
+f.write(buf);
+System.out.println("Buffer as a string");
+System.out.println(f.toString());
+System.out.println("Into array");
+byte b[] = f.toByteArray();
+for (int i=0; i<b.length; i++)
+{
+System.out.print((char) b[i]);
+}
+System.out.println("\nToan OutputStream()");
+OutputStream f2 = new FileOutputStream("test.txt");
+f.writeTo(f2);
+f2.close();
+System.out.println("Doing a reset");
+f.reset();
+for (int i=0; i<3; i++)
+f.write('b');
+System.out.println(f.toString());
+}
+}
